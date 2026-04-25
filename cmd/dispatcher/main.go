08ccea372570ef80c977c268d5147d905cbc0243
Nguyen Duy Hieu <hieund1109@gmail.com>
2026-04-25 14:42:09 +0700

    initial entry point for both binaries with dispatcher/main.go, gateway/main.go

--- cmd/dispatcher/main.go
+++ cmd/dispatcher/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/NguyenDuyHieu11/rewrite_social_media_app/internal/config"
+	"github.com/NguyenDuyHieu11/rewrite_social_media_app/internal/logger"
+)
+
+const serviceName = "dispatcher"
 
 func main() {
-	fmt.Println("dispatcher: hello")
+	cfg, err := config.Load()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "dispatcher: config load failed: %v\n", err)
+		os.Exit(1)
+	}
+
+	log := logger.New(cfg.AppEnv, cfg.LogLevel, serviceName)
+
+	log.Info("starting",
+		"addr", cfg.DispatcherAddr,
+		"config", cfg.Redacted(),
+	)
+
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
+	<-ctx.Done()
+
+	log.Info("shutdown signal received, stopping")
 }
--- cmd/gateway/main.go
+++ cmd/gateway/main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/NguyenDuyHieu11/rewrite_social_media_app/internal/config"
+	"github.com/NguyenDuyHieu11/rewrite_social_media_app/internal/logger"
+)
+
+const serviceName = "gateway"
 
 func main() {
-	fmt.Println("gateway: hello")
+	cfg, err := config.Load()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "gateway: config load failed: %v\n", err)
+		os.Exit(1)
+	}
+	log := logger.New(cfg.AppEnv, cfg.LogLevel, serviceName)
+	log.Info("starting",
+		"gateway_id", cfg.GatewayID,
+		"addr", cfg.GatewayAddr,
+		"config", cfg.Redacted(),
+	)
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+	<-ctx.Done()
+	log.Info("shutdown signal received, stopping")
 }
